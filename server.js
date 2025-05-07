f670733921486322dc2aca90f4b5f90a95b05c8d
Matheus Mösken Diegues <matheus.diegues@fatec.sp.gov.br>
2025-05-06 21:05:05 -0300

    refatorar estrutura de arquivos, atualizar dependências e ajustar estilos de layout

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.render("index");
